015efbf92ae574fcf6abd26e72428a4d18901f61
Chaapppp <chanisarappp@gmail.com>
2025-03-09 16:06:52 +0700

    Edit app.py

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 import streamlit as st
 
+st.markdown("""
+    <style>
+        div[data-testid="stSidebarNav"] ul {
+            font-size: 17px !important;  
+        }
+            
+        div[data-testid="stSidebarNav"] ul li {
+            padding: 2px 2px !important;  
+        }
+        div[data-testid="stSidebarNav"] ul li:hover, 
+        div[data-testid="stSidebarNav"] ul li:active {
+            box-shadow: 0 0 5px #FF4B4B !important;  
+            transition: box-shadow 0.4s ease-in-out        }
+    </style>
+""", unsafe_allow_html=True)
+
 project_page_1 = st.Page(
-    "pages\movie_reccomendation_page.py",
+    "pages/movie_recommend_page.py",
     title="Movie Recommendation",
     icon=":material/movie:",
 )
 project_page_2 = st.Page(    
-    "pages\emotion_page.py",
+    "pages/emotion_based_page.py",
     title="Emotion-Based Recommendation",
     icon=":material/mood:",
     default=True,
